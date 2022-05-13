ef8f29f1c9242ccd4f278a6deec0193402645193
Micha Alt <mail@lean-stack.de>
2022-05-13 13:36:55 +0200

    Fetching todos

---
+++ src/model/todo.ts
@@ -0,0 +1,8 @@
+type Todo = {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean;
+}
+
+export default Todo;
--- src/views/Todos.tsx
+++ src/views/Todos.tsx
@@ -1,3 +1,49 @@
+import { useEffect, useState } from "react";
+import Todo from "../model/todo";
+
 export default function Todos() {
-    return <h2 className="text-2xl font-semibold">Todos</h2>;
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // HTTP-Request (AJAX)
+        // Backend-Techniken: WebService, JSON-Api, RESTFull, GraphQl
+        // Client-Implementierungen im Browser: XMLHttpRequest, fetch-API
+        const promise = fetch('https://jsonplaceholder.typicode.com/todos');
+        promise
+            .then((response) => response.json())
+            .then( (data: Todo[]) => { setTodos(data)})
+            .catch(error => {})
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    return <div>
+        <h2 className="text-2xl font-semibold">Todos</h2>
+
+        { isLoading ? 
+            <p>... is loading ... </p>
+            :    
+        <table>
+            <thead>
+                <tr>
+                    <th>User</th>
+                    <th>Title</th>
+                    <th>Completed</th>
+                </tr>
+            </thead>
+            <tbody>
+                { todos.map(t =>  
+                <tr key={t.id}>
+                    <td>{t.userId}</td>
+                    <td>{t.title}</td>
+                    <td><input type="checkbox" readOnly checked={t.completed} /> </td>
+                </tr>
+                )}
+            </tbody>
+        </table>
+    }
+    </div>;
 }
